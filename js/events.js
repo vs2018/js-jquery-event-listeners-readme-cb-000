b53881c4ec929af92eddf0da50394c6d586b69f5
Vishal Sharma <vsh1819@gmail.com>
2018-08-06 07:12:00 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,44 @@
 //define functions here
 
+function getIt () {
+  $('p').on('click', function(event) {
+    alert('Hey!')
+  })
+}
+
+
+function frameIt () {
+  $('img').on('load', function(event) {
+    $(this).addClass( "tasty" );
+  })
+}
+
+function pressIt () {
+  $('form').on('keydown', function(event) {
+
+    if (event.which == 71) {
+      alert('G was pressed');
+
+    }
+})
+
+
+}
+
+function submitIt () {
+  $('form').on('submit', function(event) {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+
+
 $(document).ready(function(){
 
 // call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
